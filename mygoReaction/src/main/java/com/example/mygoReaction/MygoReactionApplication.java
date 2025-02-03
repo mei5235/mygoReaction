286ee62a151000b51caad5640dfd0b52d55fd473
Wan, Arnold SW <swan2@lpstech.com>
2025-02-03 10:28:37 +0800

    init push

---
+++ mygoReaction/src/main/java/com/example/mygoReaction/MygoReactionApplication.java
@@ -0,0 +1,13 @@
+package com.example.mygoReaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MygoReactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MygoReactionApplication.class, args);
+	}
+
+}
---
+++ mygoReaction/src/test/java/com/example/mygoReaction/MygoReactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mygoReaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MygoReactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
